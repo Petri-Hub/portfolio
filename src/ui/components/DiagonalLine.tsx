7e03f0145e326dcd718c31098d9fc88d0a292096
Fernando Petri <fernando.petri01@gmail.com>
2024-09-17 00:38:20 -0300

    feat: create diagonal line decoration component

---
+++ src/ui/components/DiagonalLine.tsx
@@ -0,0 +1,7 @@
+export default function DiagonalLine() {
+	return (
+		<span className='absolute bottom-0 right-0 aspect-square w-1/2 max-w-96'>
+			<span className='absolute  left-1/2 top-1/2 h-full w-px -translate-y-1/2  rotate-45 scale-150 bg-fourth'></span>
+		</span>
+	)
+}
